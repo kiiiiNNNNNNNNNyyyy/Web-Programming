09fbd329e7ea9c728753c82042f98e283ab956df
kiiiiNNNNNNNNNyyyy <ad.sam007@gmail.com>
2016-11-10 20:42:56 -0500

    First Commit

---
+++ server.js
@@ -0,0 +1,58 @@
+var express = require('express');
+var morgan = require('morgan'); //for showing routes status in console.
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var ejs = require('ejs');
+var ejsMate = require('ejs-mate');
+var session = require('express-session');
+var cookieParser = require('cookie-parser');
+var flash = require('express-flash');
+var MongoStore = require('connect-mongo')(session);  
+var passport= require('passport'); 
+
+var secret = require('./config/secret');
+var User = require('./models/user');
+
+var app = express();
+
+mongoose.Promise = global.Promise;
+mongoose.connect(secret.database, function(err){
+	if(err){
+		console.log(err);
+	}else{
+		console.log("Connected to the database");
+	}
+});
+
+app.use(express.static(__dirname + '/public'));
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieParser());
+app.use(session({
+	resave: true,
+	saveUninitialized: true,
+	secret: secret.secretKey,
+	store: new MongoStore({url: secret.database, autoReconnect:true}) //storing session in mongodb
+}));
+app.use(flash());
+app.use(passport.initialize());	
+app.use(passport.session());
+app.use(function(req, res, next){ //making user accesible to every route so that we dont have to mention it in every route
+	res.locals.user = req.user;
+	next();
+}); 
+
+app.engine('ejs',ejsMate);
+app.set('view engine', 'ejs');  
+
+var mainRoutes = require('./routes/main');
+var userRoutes = require('./routes/user');
+
+app.use(mainRoutes);
+app.use(userRoutes);
+
+app.listen(secret.port, function(err){
+	if(err) throw err; 
+	console.log("Server is running at port " + secret.port);
+});
